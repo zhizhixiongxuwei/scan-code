fe449203327de40bd6b69ce5e6a55e676d53b0d1
chenzhe <chenzhegodspeed@outlook.com>
2025-10-20 12:16:36 +0800

    codan和corext的增强类

---
+++ src/main/java/org/eclipse/jdt/internal/corext/dom/ASTNodes.java
@@ -0,0 +1,216 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2024 IBM Corporation and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Dmitry Stalnov (dstalnov@fusionone.com) - contributed fix for
+ *       bug "inline method - doesn't handle implicit cast" (see
+ *       https://bugs.eclipse.org/bugs/show_bug.cgi?id=24941).
+ *     Dmitry Stalnov (dstalnov@fusionone.com) - contributed fix for
+ *       bug Encapsulate field can fail when two variables in one variable declaration (see
+ *       https://bugs.eclipse.org/bugs/show_bug.cgi?id=51540).
+ *     Stephan Herrmann - Configuration for
+ *		 Bug 463360 - [override method][null] generating method override should not create redundant null annotations
+ *     Fabrice TIERCELIN - Methods to identify a signature
+ *     Pierre-Yves B. (pyvesdev@gmail.com) - contributed fix for
+ *       Bug 434747 - [inline] Inlining a local variable leads to ambiguity with overloaded methods
+ *******************************************************************************/
+package org.eclipse.jdt.internal.corext.dom;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Status;
+
+import org.eclipse.text.edits.TextEdit;
+import org.eclipse.text.edits.TextEditGroup;
+
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.Document;
+
+import org.eclipse.jdt.core.Flags;
+import org.eclipse.jdt.core.IBuffer;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IField;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IMember;
+import org.eclipse.jdt.core.ISourceReference;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.ITypeRoot;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.compiler.IProblem;
+import org.eclipse.jdt.core.compiler.ITerminalSymbols;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.AbstractTypeDeclaration;
+import org.eclipse.jdt.core.dom.Annotation;
+import org.eclipse.jdt.core.dom.AnonymousClassDeclaration;
+import org.eclipse.jdt.core.dom.ArrayAccess;
+import org.eclipse.jdt.core.dom.ArrayCreation;
+import org.eclipse.jdt.core.dom.ArrayInitializer;
+import org.eclipse.jdt.core.dom.ArrayType;
+import org.eclipse.jdt.core.dom.Assignment;
+import org.eclipse.jdt.core.dom.Block;
+import org.eclipse.jdt.core.dom.BodyDeclaration;
+import org.eclipse.jdt.core.dom.BooleanLiteral;
+import org.eclipse.jdt.core.dom.BreakStatement;
+import org.eclipse.jdt.core.dom.CastExpression;
+import org.eclipse.jdt.core.dom.CatchClause;
+import org.eclipse.jdt.core.dom.CharacterLiteral;
+import org.eclipse.jdt.core.dom.ChildListPropertyDescriptor;
+import org.eclipse.jdt.core.dom.ClassInstanceCreation;
+import org.eclipse.jdt.core.dom.Comment;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.ConditionalExpression;
+import org.eclipse.jdt.core.dom.ConstructorInvocation;
+import org.eclipse.jdt.core.dom.DoStatement;
+import org.eclipse.jdt.core.dom.EnhancedForStatement;
+import org.eclipse.jdt.core.dom.EnumConstantDeclaration;
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.ExpressionStatement;
+import org.eclipse.jdt.core.dom.FieldAccess;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.ForStatement;
+import org.eclipse.jdt.core.dom.IBinding;
+import org.eclipse.jdt.core.dom.IExtendedModifier;
+import org.eclipse.jdt.core.dom.IMethodBinding;
+import org.eclipse.jdt.core.dom.IPackageBinding;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.IVariableBinding;
+import org.eclipse.jdt.core.dom.IfStatement;
+import org.eclipse.jdt.core.dom.ImportDeclaration;
+import org.eclipse.jdt.core.dom.InfixExpression;
+import org.eclipse.jdt.core.dom.InstanceofExpression;
+import org.eclipse.jdt.core.dom.LabeledStatement;
+import org.eclipse.jdt.core.dom.LambdaExpression;
+import org.eclipse.jdt.core.dom.MemberValuePair;
+import org.eclipse.jdt.core.dom.Message;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.MethodReference;
+import org.eclipse.jdt.core.dom.Modifier;
+import org.eclipse.jdt.core.dom.Name;
+import org.eclipse.jdt.core.dom.NameQualifiedType;
+import org.eclipse.jdt.core.dom.NodeFinder;
+import org.eclipse.jdt.core.dom.NullLiteral;
+import org.eclipse.jdt.core.dom.ParameterizedType;
+import org.eclipse.jdt.core.dom.ParenthesizedExpression;
+import org.eclipse.jdt.core.dom.PostfixExpression;
+import org.eclipse.jdt.core.dom.PrefixExpression;
+import org.eclipse.jdt.core.dom.PrimitiveType;
+import org.eclipse.jdt.core.dom.QualifiedName;
+import org.eclipse.jdt.core.dom.QualifiedType;
+import org.eclipse.jdt.core.dom.RecordDeclaration;
+import org.eclipse.jdt.core.dom.ReturnStatement;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.SimpleType;
+import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
+import org.eclipse.jdt.core.dom.Statement;
+import org.eclipse.jdt.core.dom.StringLiteral;
+import org.eclipse.jdt.core.dom.StructuralPropertyDescriptor;
+import org.eclipse.jdt.core.dom.SuperConstructorInvocation;
+import org.eclipse.jdt.core.dom.SuperFieldAccess;
+import org.eclipse.jdt.core.dom.SuperMethodInvocation;
+import org.eclipse.jdt.core.dom.SwitchStatement;
+import org.eclipse.jdt.core.dom.ThisExpression;
+import org.eclipse.jdt.core.dom.ThrowStatement;
+import org.eclipse.jdt.core.dom.TryStatement;
+import org.eclipse.jdt.core.dom.Type;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.UnionType;
+import org.eclipse.jdt.core.dom.VariableDeclaration;
+import org.eclipse.jdt.core.dom.VariableDeclarationExpression;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
+import org.eclipse.jdt.core.dom.WhileStatement;
+import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+import org.eclipse.jdt.core.formatter.IndentManipulation;
+
+
+/**
+ * JDT-UI-internal helper methods that deal with {@link ASTNode}s:
+ * <ul>
+ * <li>additional operations on {@link ASTNode}s and subtypes</li>
+ * <li>finding related nodes in an AST</li>
+ * <li>some methods that deal with bindings (new such methods should go into {@link })</li>
+ * </ul>
+ */
+// @see JDTUIHelperClasses
+public class ASTNodes {
+
+
+    /**
+     * Returns a list of the direct children of a node. The siblings are ordered by start offset.
+     * @param node the node to get the children for
+     * @return the children
+     */
+    public static List<ASTNode> getChildren(ASTNode node) {
+        ChildrenCollector visitor= new ChildrenCollector();
+        node.accept(visitor);
+        return visitor.result;
+    }
+
+    private static class ChildrenCollector extends GenericVisitor {
+        public List<ASTNode> result;
+
+        public ChildrenCollector() {
+            super(true);
+            result= null;
+        }
+        @Override
+        protected boolean visitNode(ASTNode node) {
+            if (result == null) { // first visitNode: on the node's parent: do nothing, return true
+                result= new ArrayList<>();
+                return true;
+            }
+            result.add(node);
+            return false;
+        }
+    }
+
+
+
+    /**
+     * Returns <code>true</code> iff <code>parent</code> is a true ancestor of <code>node</code>
+     * (i.e. returns <code>false</code> if <code>parent == node</code>).
+     *
+     * @param node node to test
+     * @param parent assumed parent
+     * @return <code>true</code> iff <code>parent</code> is a true ancestor of <code>node</code>
+     */
+    public static boolean isParent(ASTNode node, ASTNode parent) {
+        Assert.isNotNull(parent);
+        do {
+            node= node.getParent();
+            if (node == parent)
+                return true;
+        } while (node != null);
+        return false;
+    }
+
+
+
+}
+
---
+++ src/main/java/org/eclipse/jdt/internal/ui/util/ASTHelper.java
@@ -0,0 +1,117 @@
+/*******************************************************************************
+ * Copyright (c) 2019, 2025 IBM Corporation and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.internal.ui.util;
+
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.Modifier;
+
+@SuppressWarnings("deprecation")
+public class ASTHelper {
+
+    public static final int JLS2 = AST.JLS2;
+    public static final int JLS3 = AST.JLS3;
+    public static final int JLS4 = AST.JLS4;
+    public static final int JLS8 = AST.JLS8;
+    public static final int JLS9 = AST.JLS9;
+    public static final int JLS10 = AST.JLS10;
+    public static final int JLS11 = AST.JLS11;
+    public static final int JLS12 = AST.JLS12;
+    public static final int JLS13 = AST.JLS13;
+    public static final int JLS14 = AST.JLS14;
+    public static final int JLS15 = AST.JLS15;
+    public static final int JLS16 = AST.JLS16;
+    public static final int JLS17 = AST.JLS17;
+    public static final int JLS18 = AST.JLS18;
+    public static final int JLS19 = AST.JLS19;
+    public static final int JLS20 = AST.JLS20;
+    public static final int JLS21 = AST.JLS21;
+    public static final int JLS22 = AST.JLS22;
+    public static final int JLS23 = AST.JLS23;
+    public static final int JLS24 = AST.JLS24;
+
+    private static boolean isNodeTypeSupportedInAST(AST ast, int nodeType) {
+        switch (nodeType) {
+            case ASTNode.SWITCH_CASE:
+            case ASTNode.SWITCH_EXPRESSION:
+            case ASTNode.YIELD_STATEMENT:
+                return ast.apiLevel() >= JLS14;
+            case ASTNode.TEXT_BLOCK:
+                return ast.apiLevel() >= JLS15;
+            case ASTNode.RECORD_DECLARATION:
+            case ASTNode.INSTANCEOF_EXPRESSION:
+                return ast.apiLevel() >= JLS16;
+            case ASTNode.TAG_PROPERTY:
+                return ast.apiLevel() >= AST.JLS18;
+            case ASTNode.TYPE_PATTERN:
+            case ASTNode.RECORD_PATTERN:
+            case ASTNode.PATTERN_INSTANCEOF_EXPRESSION:
+                return ast.apiLevel() >= JLS21;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    private static boolean isModifierSupportedInAST(AST ast, int modifier) {
+        switch (modifier) {
+            case Modifier.SEALED:
+            case Modifier.NON_SEALED:
+                return ast.apiLevel() >= JLS17;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    public static boolean isYieldNodeSupportedInAST(AST ast) {
+        return isNodeTypeSupportedInAST(ast, ASTNode.YIELD_STATEMENT);
+    }
+
+    public static boolean isSwitchExpressionNodeSupportedInAST(AST ast) {
+        return isNodeTypeSupportedInAST(ast, ASTNode.SWITCH_EXPRESSION);
+    }
+
+    public static boolean isTextBlockNodeSupportedInAST(AST ast) {
+        return isNodeTypeSupportedInAST(ast, ASTNode.TEXT_BLOCK);
+    }
+
+    public static boolean isSwitchCaseExpressionsSupportedInAST(AST ast) {
+        return isNodeTypeSupportedInAST(ast, ASTNode.SWITCH_CASE);
+    }
+
+    public static boolean isRecordDeclarationNodeSupportedInAST(AST ast) {
+        return isNodeTypeSupportedInAST(ast, ASTNode.RECORD_DECLARATION);
+    }
+
+    public static boolean isSealedTypeSupportedInAST(AST ast) {
+        return isModifierSupportedInAST(ast, Modifier.SEALED);
+    }
+
+    public static boolean isInstanceofExpressionPatternSupported(AST ast) {
+        return isNodeTypeSupportedInAST(ast, ASTNode.INSTANCEOF_EXPRESSION);
+    }
+
+    public static boolean isPatternSupported(AST ast) {
+        return isNodeTypeSupportedInAST(ast, ASTNode.TYPE_PATTERN);
+    }
+
+    public static boolean isRecordPatternSupported(AST ast) {
+        return isNodeTypeSupportedInAST(ast, ASTNode.RECORD_PATTERN);
+    }
+
+    public static boolean isJavaDocCodeSnippetSupported(AST ast) {
+        return isNodeTypeSupportedInAST(ast, ASTNode.TAG_PROPERTY);
+    }
+}
