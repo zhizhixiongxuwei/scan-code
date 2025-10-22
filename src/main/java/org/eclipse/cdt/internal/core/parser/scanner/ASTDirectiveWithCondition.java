4da303767eb35af2f8d281b5cd0ebe816f30bfee
chenzhe <chenzhegodspeed@outlook.com>
2025-10-22 11:30:08 +0800

    ASTPreprocessorNode大类拆分

---
+++ src/main/java/org/eclipse/cdt/internal/core/parser/scanner/ASTDirectiveWithCondition.java
@@ -0,0 +1,27 @@
+package org.eclipse.cdt.internal.core.parser.scanner;
+
+import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+
+public abstract class ASTDirectiveWithCondition extends ASTPreprocessorNode {
+    protected final int fConditionOffset;
+    private final boolean fTaken;
+
+    public ASTDirectiveWithCondition(IASTTranslationUnit parent, int startNumber, int condNumber, int endNumber,
+                                     boolean taken) {
+        super(parent, IASTTranslationUnit.PREPROCESSOR_STATEMENT, startNumber, endNumber);
+        fConditionOffset = condNumber;
+        fTaken = taken;
+    }
+
+    public boolean taken() {
+        return fTaken;
+    }
+
+    public String getConditionString() {
+        return new String(getSource(fConditionOffset, getOffset() + getLength() - fConditionOffset));
+    }
+
+    public char[] getCondition() {
+        return getConditionString().toCharArray();
+    }
+}
